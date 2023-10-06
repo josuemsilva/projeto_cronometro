6935e4c00673d4578e1ac41890158f136cbc774f
JooJC <joojmoutinho45@gmail.com>
2023-10-05 23:33:24 -0300

    deploy do projeto

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: "COMEÇAR",
+    };
+    this.timer = null;
+    this.comecar = this.comecar.bind(this);
+    this.limpar = this.limpar.bind(this);
+  }
+
+  comecar() {
+    let state = this.state;
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = "COMEÇAR";
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      }, 100);
+      state.botao = "PAUSAR";
+    }
+    this.setState(state);
+  }
+
+  limpar() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    let state = this.state;
+    state.numero = 0;
+    state.botao = "COMEÇAR";
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <img src={require("./assets/cronometro.png")} alt="" className="img" />
+        <a href="$" className="timer">
+          {this.state.numero.toFixed(1)}
+        </a>
+        <div className="areaBtn">
+          <a href="$" className="botao" onClick={this.comecar}>
+            {this.state.botao}
+          </a>
+          <a href="$" className="botao" onClick={this.limpar}>
+            LIMPAR
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
